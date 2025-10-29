d4e40b3902b39fa00bfbefba20d1c327444f4940
Anil krishna <anilkumar.konasalekrishna@aveva.com>
2025-10-28 17:22:57 -0700

    rect in form

--- deskApps/TabsFormApp/Form1.cs
+++ deskApps/TabsFormApp/Form1.cs
@@ -18,6 +18,8 @@ public partial class Form1 : Form
         tabControl.Name = "mainTabControl";
         tabControl.Location = new Point(10, 10);
         tabControl.Size = new Size(400, 300);
+        tabControl.Dock = DockStyle.Fill;
+        tabControl.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
 
         // Create and add TabPages and their contents...
         TabPage dataTab = new TabPage("Data View");
@@ -30,9 +32,12 @@ public partial class Form1 : Form
         // Add the TabControl to the Form
         this.Controls.Add(tabControl);
 
+        int numberOfTabs = 20;
+
         // 2. Loop 'n' times to create and configure each TabPage
         for (int i = 1; i <= numberOfTabs; i++)
         {
+            int labelNumber = i;
             // a. Create a new TabPage object
             TabPage newPage = new TabPage();
 
@@ -42,11 +47,23 @@ public partial class Form1 : Form
             // c. Set the tab's Name (useful for code reference)
             newPage.Name = $"tabPage_{i}";
 
+            Panel coloredRectPanel = new Panel();
+    coloredRectPanel.Name = $"panelRect_{labelNumber}";
+    coloredRectPanel.Location = new Point(50, 50 + (1 * 80)); // Position each panel below the last
+    coloredRectPanel.Size = new Size(250, 60); // Set the size of your rectangle
+    coloredRectPanel.BackColor = Color.LightSkyBlue; // Set the fill color for the rectangle
+    coloredRectPanel.BorderStyle = BorderStyle.FixedSingle;
+
             // d. OPTIONAL: Add some content to the TabPage
             Label label = new Label();
             label.Text = $"Content for {newPage.Text}";
             label.Location = new System.Drawing.Point(10, 10);
-            newPage.Controls.Add(label);
+            label.Size = new System.Drawing.Size(600, 20);
+            label.Dock = DockStyle.Fill;
+            label.AutoSize = false;
+
+            coloredRectPanel.Controls.Add(label);
+            newPage.Controls.Add(coloredRectPanel);
 
             // e. Add the new TabPage to the TabControl's collection
             tabControl.TabPages.Add(newPage);
@@ -55,7 +72,7 @@ public partial class Form1 : Form
 
     // 2. Your Event Handler
     private void LoadButton_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Data is loading...", "Action Performed");
-        }
+    {
+        MessageBox.Show("Data is loading...", "Action Performed");
+    }
 }
